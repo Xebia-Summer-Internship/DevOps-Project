106d3450cdb48e9f4668bcdf5498f66f55583dc8
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-06-18 23:44:11 +0530

    updates

--- server/Controllers/hotelAuthController.js
+++ server/Controllers/hotelAuthController.js
@@ -1,37 +1,36 @@
-const owner = require('../models/hotelOwner')
-const bcrypt = require('bcrypts')
-const jwt =require('jsonwebtoken')
+const hotel = require("../models/hotelOwner");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const hotelOwnerSignUp=(req,res,next)=>{
-    bcrypt.hash(req.body.password,10,function(err, hashedPass){
-        if(err){
-            res.json({
-                error:err
-            })
-        }
+const hotelOwnerSignUp = async (req, res, next) => {
+  const salt = await bcrypt.genSalt(10);
+  const secPass = await bcrypt.hash(req.body.password, salt);
+
+  let owner = new hotel({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    FassaiID: req.body.fssaiId,
+    password: secPass,
+    address: req.body.address,
+    pincode: req.body.pincode,
+  });
+
+  owner
+    .save()
+    .then((owner) => {
+      res.json({
+        message: "Owner Added Successfully",
+      });
     })
-    let owner = new hotel ({
-        name: req.body.name,
-        email:req.body.email,
-        phone: req.body.phone,
-        FssaiID: req.body.phone,
-        password: hashedPAss,
-        address: req.body.phone,
-        pincode: req.body.phone,
-        
-    })
-    owner.save()
-    .then(owner=>{
-        res.json({
-            message: 'Owner Added Successfully'
-        })
-    })
-    .catch(error =>{
-        res.json({
-            message: 'An error occured!'
-        })
-    })
-}
-module.exports= {
-    hotelOwnerSignUp
-}
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: "An error occured!",
+      });
+    });
+};
+
+module.exports = {
+  hotelOwnerSignUp,
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const AuthController = require("../controllers/authController");
+const HotelAuthController = require("../controllers/hotelAuthController");
 
 // router.get("/", (req, res) => {
 //   res.send("route is working fine;");
@@ -10,5 +11,6 @@ const AuthController = require("../controllers/authController");
 
 router.post("/customerSignUp", AuthController.customerSignUp);
 router.get("/test", AuthController.testFun);
+router.post("/hotelOwnerSignUp", HotelAuthController.hotelOwnerSignUp);
 
 module.exports = router;
