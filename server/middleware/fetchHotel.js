c970fbb38f0b017c72494008f9b06a04abd9e3ed
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-06-19 14:51:28 +0530

    added middleware, tested login controllers

---
+++ server/middleware/fetchHotel.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const fetchHotel = (req, res, next) => {
+  const token = req.headers["x-access-token"];
+  console.log(token);
+  if (!token) {
+    return res.status(401).json({
+      message: "No token provided.",
+    });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: "Invalid token.",
+      });
+    }
+    console.log(decoded);
+    req.hotel = decoded;
+    next();
+  });
+};
+
+module.exports = fetchHotel;
