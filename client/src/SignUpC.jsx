0ad06e06b0b3d345a425599a9ee7453d00dad61c
kartikeya532001 <kartikeya532001@gmail.com>
2022-06-22 10:45:19 +0530

    Sign Up UI Updated

--- client/src/SignUpC.jsx
+++ client/src/SignUpC.jsx
@@ -9,12 +9,16 @@ function SignUpC () {
     return (
         <>
         <Nav />
-            <div id='forms'>
-                <h1 className='form-heading'>Sign Up</h1>
-              <Link to='/SignUpC'> <button id ='Customer'>As Customer</button> </Link>
-               <Link to='/SignUpH'> <button id ='Hotel'>As Hotel</button> </Link>
-                
+    <div className='Cont'>
+    <div className='Signup'>
+        <div id='forms'>
+                <h1 className='form-heading'>Sign Up</h1> <br />
+            <div className='div1'>
+                <div > <Link to='/SignUpC'> <button id ='Customer'>As Customer</button> </Link> </div>
+                <div > <Link to='/SignUpH'> <button id ='Hotel'>As Hotel</button> </Link>  </div> 
+            </div>
                 <form className='form' >
+                <br></br>
                     <input type = 'text' placeholder='Enter your name' name='name'  id="yourName"></input>
                     <input type = 'email' placeholder="Enter your email" name='email'  id="email"></input><br /><br/>
                     <input type = 'text' placeholder="Enter phone number" name='phoneNumber'  id="phoneNumber"></input>
@@ -23,10 +27,11 @@ function SignUpC () {
                     <input type = 'text' placeholder="Confirm Password" name='conPassword'  id="confirmPassword"></input><br></br>
                     <button type='submit'>Submit</button>
                     <br></br><br></br>
-                    <p id="accountExists">Already have an account?<Link to ="/"id='signInBtn'> Sign in</Link></p>
+                    <p id="accountExists"><b>Already have an account?</b><Link to ="/"id='signInBtn'> <b>Sign in</b></Link></p>
                 </form>
-            </div>
-
+        </div>
+    </div>
+    </div>
         </>
     );
 }
--- client/src/SignUpH.jsx
+++ client/src/SignUpH.jsx
@@ -1,7 +1,7 @@
-// Page for SignUp page for Hotel.
-
+// Page for SignUp Page For User.
 import React from 'react';
 import './SignUp.css';
+import './SignUpH.css';
 import Nav from './Nav';
 import { Link } from 'react-router-dom';
 
@@ -9,24 +9,31 @@ function SignUpH () {
     return (
         <>
         <Nav />
-            <div id='forms'>
-                <h1 className='form-heading'>Sign Up</h1>
-                <Link to='/SignUpC'> <button id ='Customer1'>As Customer</button> </Link>
-                <Link to='/SignUpH'> <button id ='Hotel1'>As Hotel</button> </Link>
+    <div className='Cont'>
+    <div className='Signup1'>
+        <div id='forms'>
+                <h1 className='form-heading'>Sign Up</h1> <br />
+            <div className='div1'>
+                <div > <Link to='/SignUpC'> <button id ='Customer1'>As Customer</button> </Link> </div>
+                <div > <Link to='/SignUpH'> <button id ='Hotel1'>As Hotel</button> </Link>  </div> 
+            </div>
                 <form className='form' >
-                    <input type = 'text' placeholder='Enter your name' name='name'  id="yourName"></input>
+                <br></br>
+                <input type = 'text' placeholder='Enter your name' name='name'  id="yourName"></input>
                     <input type = 'email' placeholder="Enter your email" name='email'  id="email"></input><br /><br/>
                     <input type = 'text' placeholder="Enter phone number" name='phoneNumber'  id="phoneNumber"></input>
                     <input type = 'text' placeholder="Enter your pincode" name='DOB'  id="DOB"></input><br /><br/>
-                    <input type = 'text' placeholder="Enter hotel ID" name='phoneNumber'  id="phoneNumber"></input>
-                    <input type = 'text' placeholder="Enter fssai license number" name='phoneNumber'  id="DOB"></input><br /><br/>
+                    <input type = 'text' placeholder="Enter hotel ID" name='phoneNumber'  id="hotelID"></input>
+                    <input type = 'text' placeholder="Enter fssai license number" name='phoneNumber'  id="license"></input><br /><br/>
                     <input type = 'text' placeholder="Create Your Password" name='password'  id="password"></input>
                     <input type = 'text' placeholder="Confirm Password" name='conPassword'  id="confirmPassword"></input><br></br>
                     <button type='submit'>Submit</button>
                     <br></br><br></br>
-                    <p id="accountExists">Already have an account?<Link to ="/" id='signInBtn'> Sign in</Link></p>
+                    <p id="accountExists"><b>Already have an account?</b><Link to ="/"id='signInBtn'> <b>Sign in</b></Link></p>
                 </form>
-            </div>
+        </div>
+    </div>
+    </div>
         </>
     );
 }
