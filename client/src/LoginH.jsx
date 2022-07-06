322a3f750edcc9e959966814cd5c4a4ce98db9f3
aman <pantaman98@gmail.com>
2022-07-06 21:57:25 +0530

    INTEGRATED LOGIN FOR HOTEL

--- client/src/LoginH.jsx
+++ client/src/LoginH.jsx
@@ -1,5 +1,5 @@
 // Page for Login Page.
-import React from 'react';
+import React,{useState} from 'react';
 import Nav from './Nav';
 import './Login.css';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,48 @@ import './LoginH.css'
 
 
 function LoginH () {
+
+    const [credentials, setCredentials] = useState({email:"",password:""});
+
+    const onChange = (e)=>{
+        setCredentials({...credentials,[e.target.name]:e.target.value})
+    }
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault()
+        console.log(credentials);
+        
+        var dataObj = {
+            email: credentials.email,
+            password: credentials.password,
+        }       
+
+        // console.log(JSON.stringify(awesomeObj));
+       
+        var response = await fetch('/api/customerSignIn', {
+            method: 'POST',
+            mode: 'no-cors',
+            cache: 'no-cache',
+            accessControlAllowOrigin: 'http://localhost:5000,*',
+            accept: 'application/json',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(dataObj)
+        });
+        var json = await response.json().catch(err => console.log(err));
+        
+        console.log("response received: " + JSON.stringify(json));
+
+        if(json.success){            
+            localStorage.setItem('access-token',json.jwtToken);
+            // handle success - redirect to home page
+            // Redirect Here
+        }
+        else{
+            //alert to show error to be handeled by frontend
+        }
+
+    }
+
     return (
         <>
             <Nav />
@@ -18,10 +60,10 @@ function LoginH () {
                         <Link to='/LoginH'> <button id ='Hotel3'>Hotel</button> </Link> 
                     </div> 
                     <br /><br />
-                    <input type = 'email' placeholder="Enter your email" name='email'  id="email1" required /><br /><br/>
-                    <input type = 'password' placeholder="Enter your Password" name='password'  id="password1" required /> <br /> <br />
+                    <input type = 'email' placeholder="Enter your email" name='email'  id="email1" onChange={onChange}required /><br /><br/>
+                    <input type = 'password' placeholder="Enter your Password" name='password'  id="password1" onChange={onChange}required /> <br /> <br />
                     {/*<Link to ="" id="forgot"><b>Forgot Password?</b></Link> <br />*/}
-                    <button type='submit' id="button">Login</button>
+                    <button type='submit' id="button" onClick={handleOnSubmit}>Login</button>
                     <br></br><br></br>
                     <p id="accountExists1"><b>Don't have an account?</b><Link to ="/SignUpC" id='signInBtn1'><b> Sign Up</b></Link></p>
                 </div>
