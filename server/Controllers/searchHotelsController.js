7324396c39ca35168132bc0ff90806f99b43b6c4
sekhar76405 <ssjio5402@gmail.com>
2022-07-11 19:34:46 +0530

    returning the list of hotel with the given pincode

--- server/Controllers/searchHotelsController.js
+++ server/Controllers/searchHotelsController.js
@@ -1,9 +1,20 @@
+const hotelSchema = require('../models/hotelOwner');
 
-const searchHotels = async (req, res, next) => {
+const searchHotels = async (req, res) => {
 
     console.log(req.body);
-    res.json({message: "Hotels"});
 
+    const hotels = await hotelSchema.find({"pincode": req.body.pincode});
+    console.log(hotels);
+    if(hotels == null){
+      res.json({message: "Hotels not found"});
+    }
+    else{
+      res.json(hotels)
+    }
+    // const hotels = await hotelSchema.find();
+    // res.json(hotels);
+    
 };
 
 
